7e99fffdfe6e19628a5082fae4d95c67bee6af90
takeuchi-y <takeuchi.y@opst.co.jp>
2020-02-28 14:42:01 +0900

    ログインできるようになった

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://127.0.0.1:8000');
-  await page.screenshot({path: 'hello.png'});
+const TEST_URL = 'http://127.0.0.1:8000';
+const USER_NAME = ''; // TODO 外だしする
+const PASSWORD = '';  // TODO 外だしする
 
+(async () => {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+
+  await page.goto(TEST_URL);
+  await page.screenshot({path: `01-welcome-page.png`});
+  await page.click('a[href="/login"]');
+
+  await page.screenshot({path: `02-login.png`});
+  await page.type('input[name="user-name"]', USER_NAME);
+  await page.type('input[name="password"]', PASSWORD);
+  await page.click('input[value="ログイン"]');
+
+  await page.screenshot({path: `03-user-home.png`});
   await browser.close();
 })();
