349a3e177a8a4674c7f4e57fcb0c46999e10d830
takeuchi-y <takeuchi.y@opst.co.jp>
2020-02-28 14:53:50 +0900

    センシティブな情報を.envに持たせた

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
+require('dotenv').config();
 
-const TEST_URL = 'http://127.0.0.1:8000';
-const USER_NAME = ''; // TODO 外だしする
-const PASSWORD = '';  // TODO 外だしする
+const TEST_URL = process.env.TEST_URL;
+const LOGIN_USER_NAME = process.env.LOGIN_USER_NAME;
+const LOGIN_USER_PASSWORD = process.env.LOGIN_USER_PASSWORD;
 
 (async () => {
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
   await page.goto(TEST_URL);
@@ -13,8 +14,8 @@ const PASSWORD = '';  // TODO 外だしする
   await page.click('a[href="/login"]');
 
   await page.screenshot({path: `02-login.png`});
-  await page.type('input[name="user-name"]', USER_NAME);
-  await page.type('input[name="password"]', PASSWORD);
+  await page.type('input[name="user-name"]', LOGIN_USER_NAME);
+  await page.type('input[name="password"]', LOGIN_USER_PASSWORD);
   await page.click('input[value="ログイン"]');
 
   await page.screenshot({path: `03-user-home.png`});
